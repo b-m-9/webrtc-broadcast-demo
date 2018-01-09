10edd9cbedc92fb198d6b705b74167a01d044efb
medve-dev <200.medvedev@gmail.com>
2018-01-10 00:07:18 +0200

    fix

--- server.js
+++ server.js
@@ -1,56 +1,11 @@
 const fs = require("fs");
 const path = require('path');
-
+var static_file = require('node-static');
+var file = new static_file.Server('./www');
 let app = require('http').createServer((request, response) => {
-    let uri = require('url').parse(request.url).pathname,
-        filename = path.join(process.cwd(), uri);
-
-    let isWin = !!process.platform.match(/^win/);
-
-    if (!fs.existsSync(filename)) {
-        response.writeHead(404, {
-            "Content-Type": "text/plain"
-        });
-        response.write('404 Not Found: ' + filename + '\n');
-        response.end();
-        return false;
-    }
-
-    let head = undefined;
-
-    if (fs.statSync(filename).isDirectory()) {
-        if (!isWin) filename += '/index.html';
-        else filename += '\\index.html';
-        head = {
-            "Content-Type": "text/html"
-        }
-    }
-
-    fs.exists(filename, (exists) => {
-        if (!exists) {
-            response.writeHead(404, {
-                "Content-Type": "text/plain"
-            });
-            response.write('404 Not Found: ' + filename + '\n');
-            response.end();
-            return false;
-        }
-
-        fs.readFile(filename, 'binary', (err, file) => {
-            if (err) {
-                response.writeHead(500, {
-                    "Content-Type": "text/plain"
-                });
-                response.write(err + "\n");
-                response.end();
-                return false;
-            }
-            response.writeHead(200, head);
-            response.write(file, 'binary');
-            response.end();
-            return true;
-        });
-    });
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
 });
 
 app = app.listen(process.env.PORT || 8888, process.env.IP || "0.0.0.0", () => {
